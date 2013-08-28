0f1e6bdcd3acb972a1945a3f4e890932e84996b5
jlhonora <jlhonora@ing.puc.cl>
2013-08-28 11:00:46 -0400

    Added a cover letter, better build system

--- build.sh
+++ build.sh
@@ -1,2 +1,37 @@
 #!/usr/bin/env bash
-xelatex resume.tex
+
+build_all=0
+build_resume=0
+build_cover=0
+
+case "$1" in
+resume )
+	build_resume=1 
+	;;
+cover )
+	build_cover=1 
+	;;
+* )
+	build_resume=1
+	build_cover=1
+	build_all=1 
+	;;
+esac
+	
+if [ $build_resume -gt 0 ]
+then
+	echo "Building resume"
+	xelatex resume.tex
+fi
+
+if [ $build_cover -gt 0 ]
+then
+	echo "Building cover letter"
+	xelatex cover.tex
+fi
+
+if [ $build_all -gt 0 ]
+then
+	echo "Stitching"
+	xelatex all.tex
+fi
